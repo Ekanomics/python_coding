9c0a9b68be83b0d5c2da33579504fd6c1d547162
Erkin <karikeev@yahoo.com>
2025-05-06 00:27:55 -0500

    day1

---
+++ dailycoding/exercism.exercises.py
@@ -0,0 +1,20 @@
+# # 1)
+# elapsed_bake_time = int(input("Enter "))
+# EXPECTED_BAKE_TIME = 30
+# def bake_time_remaining(elapsed_bake_time):
+#     return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+# print(bake_time_remaining(elapsed_bake_time))
+
+
+# preparation_time = 2
+# def preparation_time_in_minutes(number_of_layers):
+#     return number_of_layers * preparation_time
+
+# print(preparation_time_in_minutes(3))
+
+
+# def elapsed_time_in_minutes (number_of_layers, elapsed_bake_time):
+#     return preparation_time + elapsed_bake_time
+
+# print(elapsed_time_in_minutes(3, 20))
