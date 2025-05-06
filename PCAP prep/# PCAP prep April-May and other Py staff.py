742d684e5074fe67acbbd1396eac8158aabc55c1
Erkin <karikeev@yahoo.com>
2025-05-05 21:05:43 -0500

    daily_coding_trials

--- PCAP prep/# PCAP prep April-May and other Py staff.py
+++ PCAP prep/# PCAP prep April-May and other Py staff.py
@@ -1,21 +1,23 @@
                 # PCAP prep April-May and other Py staff 
 
-EXPECTED_BAKE_TIME = 40
-def bake_time_remaining(elapsed_bake_time):
-    return EXPECTED_BAKE_TIME - elapsed_bake_time
+# EXPECTED_BAKE_TIME = 40
+# def bake_time_remaining(elapsed_bake_time):
+#     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-bake_time_remaining()
-
-
-
-preparation_time = 2
-def preparation_time_in_minutes(number_of_layers):
-    return number_of_layers * preparation_time
-
-preparation_time()
+# bake_time_remaining()
 
 
-MIWKA, 
 
-def elapsed_bake_in_minutes (preparation_time, bake, time):
-    return preparation_time
+# preparation_time = 2
+# def preparation_time_in_minutes(number_of_layers):
+#     return number_of_layers * preparation_time
+
+# preparation_time()
+
+
+# def elapsed_bake_in_minutes (preparation_time, bake, time):
+#     return preparation_time
+
+
+
+
