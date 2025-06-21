91f9850c8b893f566b80557305f1ffbb88fd4d16
Erkin <karikeev@yahoo.com>
2025-06-21 14:17:39 -0500

    task 3 py from codewars, gonna continue late in the evening

--- dailycoding/codewars.py
+++ dailycoding/codewars.py
@@ -24,9 +24,53 @@
 
 # def summation(num):
 #     return sum(range(1, num + 1))
-
 # print(summation(8))
 
 # ____________________________________
 
 # 3) 
+# def operation(a):
+#     return a()
+# def function(arg):
+#     print (1, 2, 3, 4)
+# function()
+# print(function)
+
+
+# def my_function():
+#   print("Hello from a function")
+# my_function()
+
+
+# def my_function(fname):
+#   print(fname + " Refsnes")
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+# Examples(Operator, value1, value2) --> output
+
+def function(arg, value1, value2):
+  if arg == '+':
+    return value1 + value2
+  elif arg == '-':
+    return value1 - value2
+  elif arg == '*':
+    return value1 * value2
+  elif arg == '/':
+    return value1 / value2
+  else:
+    raise ValueError("Invalid argument operation")
+  
+print(function('+', 2, 3))
+print(function('-', 2, 3))
+print(function('*', 2, 3))
+print(function('/', 2, 3))
+
+
+
