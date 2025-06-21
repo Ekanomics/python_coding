560e4d94671663183e5277c0514522330942d611
Erkin <karikeev@yahoo.com>
2025-06-21 01:07:55 -0500

    daily coding, tried codewars tasks

---
+++ dailycoding/codewars.py
@@ -0,0 +1,32 @@
+# ____________________________________
+
+# 1) Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+
+# def boolean_to_string(b):
+#     return str(b)
+
+# print(boolean_to_string(True))
+# print(boolean_to_string(False))
+
+# Solution: This works because Python’s built-in str() function automatically converts boolean values to the strings "True" or "False".
+# ____________________________________
+
+# 2) Summation
+# Write a program that finds the summation of every number from 1 to num (both inclusive). 
+# The number will always be a positive integer greater than 0. Your function only needs to return the result, 
+# what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+# For example (Input -> Output):
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+
+# def summation(num):
+#     return sum(range(1, num + 1))
+
+# print(summation(8))
+
+# ____________________________________
+
+# 3) 
