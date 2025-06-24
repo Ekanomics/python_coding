e91cb1bfd0e807808d2d44d09d7b862c333d8a59
Erkin <karikeev@yahoo.com>
2025-06-24 00:20:41 -0500

    codewars task 7 fundamentals added

--- dailycoding/codewars.py
+++ dailycoding/codewars.py
@@ -164,3 +164,26 @@
 
 # ______________________________________________
 # Task 7)
+# Write function bmi that calculates body mass index (bmi = weight / height2).
+# if bmi <= 18.5 return "Underweight"
+# if bmi <= 25.0 return "Normal"
+# if bmi <= 30.0 return "Overweight"
+# if bmi > 30 return "Obese"
+
+
+# Solution:
+def bmi(weight, height):
+    x = (weight) / (height ** 2)
+    if x <= 18.5:
+        return "Underweight"
+    elif x <= 25.0:
+        return "Normal"
+    elif x <= 30.0:
+        return "Overweight"
+    else:
+        return "Obese"
+
+print(bmi(77, 1.72))
+
+# test  = (77 / (172*172)) * 10000
+# print(test)
